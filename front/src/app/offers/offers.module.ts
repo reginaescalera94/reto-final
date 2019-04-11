903cd5723f7914e1e7866ff1ae55aa0eb7ef6419
Sergio Raposo Vargas <sraposov@everis.com>
2019-04-11 23:17:54 +0200

    Menu + componente vacío

---
+++ front/src/app/offers/offers.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OfferListComponent } from './offer-list/offer-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OfferListComponent]
+})
+export class OffersModule { }
